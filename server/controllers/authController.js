130819d3c5167c88a49f12e26f6bdbc2cb5c76bb
mohamed nagah <mohamed.nagah95@gmail.com>
2020-06-08 17:12:51 +0200

    finished the authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const passport = require('passport');
 
 exports.validateSignup = (req, res, next) => {
     req.sanitizeBody('name');
@@ -36,8 +37,24 @@ exports.signup = async (req, res) => {
     })
 };
 
-exports.signin = () => {};
+exports.signin = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) return res.status(500).json(err.message);
+        if (!user) return res.status(400).json(info.message);
+        req.login(user, (err) => {
+            if (err) return res.status(400).json(err.message);
+            res.json(user);
+        })
+    })(req, res, next);
+};
 
-exports.signout = () => {};
+exports.signout = (req, res, next) => {
+    res.clearCookie('next-cookie.sid');
+    req.logout();
+    res.json({message: 'You are signed out!'})
+};
 
-exports.checkAuth = () => {};
+exports.checkAuth = () => {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/signin');
+};
