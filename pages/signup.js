6076fadb5d4acd866049b135096958ddb7f78133
mohamed nagah <mohamed.nagah95@gmail.com>
2020-06-22 11:08:11 +0200

    finished the signup page

--- pages/signup.js
+++ pages/signup.js
@@ -16,28 +16,60 @@ import Gavel from "@material-ui/icons/Gavel";
 import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
 import withStyles from "@material-ui/core/styles/withStyles";
 import {signUpUser} from "../lib/auth";
+import Link from 'next/link';
+
+function Transition(props) {
+  return <Slide direction='up' {...props}/>;
+}
 
 class Signup extends React.Component {
   state = {
     name: '',
     email: '',
-    password: ''
+    password: '',
+    error: '',
+    openError: false,
+    openSuccess: false,
+    isLoading: false,
+    createdUser: ''
   };
+
+  handleClose = () => {
+    this.setState({openError: false});
+  }
 
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value});
   };
 
+  showError = (err) => {
+    const error = err.response && err.response.data || err.message;
+    this.setState({openError: true, error, isLoading: false, createdUser: ''});
+  }
+
   handleSubmit = (event) => {
     event.preventDefault();
     const {name, email, password} = this.state;
 
+    this.setState({isLoading: true, error: ''});
+
     const user = {name, email, password};
-    signUpUser(user);
+    signUpUser(user)
+        .then(createdUser => {
+          console.log(createdUser);
+          this.setState({
+            isLoading: false,
+            createdUser,
+            error: '',
+            openSuccess: true
+          })
+        })
+        .catch(this.showError);
   }
 
   render() {
     const {classes} = this.props;
+    const {error, openError, openSuccess, createdUser, isLoading} = this.state;
     return (
         <div className={classes.root}>
           <Paper className={classes.paper}>
@@ -49,24 +81,53 @@ class Signup extends React.Component {
             </Typography>
             <form className={classes.form} onSubmit={this.handleSubmit}>
               <FormControl margin='normal' required fullWidth>
-                <InputLabel htmlFor='name'>Name</InputLabel>
-                <input type="text" name='name' onChange={this.handleChange}/>
+                <InputLabel htmlFor="name">Name</InputLabel>
+                <Input type="text" id='name' name='name' onChange={this.handleChange}/>
               </FormControl>
               <FormControl margin='normal' required fullWidth>
                 <InputLabel htmlFor='email'>Email</InputLabel>
-                <input type="email" name='email' onChange={this.handleChange}/>
+                <Input type="email" name='email' onChange={this.handleChange}/>
               </FormControl>
               <FormControl margin='normal' required fullWidth>
                 <InputLabel htmlFor='password'>Password</InputLabel>
-                <input type="password" name='password' onChange={this.handleChange}/>
+                <Input type="password" name='password' onChange={this.handleChange}/>
               </FormControl>
               <Button type='submit'
                       fullWidth
                       variant='contained'
                       color='primary'
-                      className={classes.submit}>Sign Up</Button>
+                      className={classes.submit}>{isLoading ? 'Signing Up...' : 'Sign Up'}</Button>
             </form>
+            {
+              error && <Snackbar
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right'
+                  }}
+                  open={openError}
+                  onClose={this.handleClose}
+                  autoHideDuration={6000}
+                  message={<span className={classes.snack}>{error}</span>}
+              />
+            }
           </Paper>
+
+          <Dialog open={openSuccess} disableBackdropClick={true} TransitionComponent={Transition}>
+            <DialogTitle>
+              <VerifiedUserTwoTone className={classes.icon}/>
+              New Account
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText>User {createdUser} successfully created!</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button color='primary' variant='contained'>
+                  <Link href='/signin'>
+                    <a className={classes.signinLink}>sign in</a>
+                  </Link>
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
     )
   }
